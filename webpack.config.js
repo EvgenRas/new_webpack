d9d2ab1895ee0483a44e6d773fe188a17696d742
Evgeniy Rassolov <tazij@yandex.ru>
2021-12-06 19:38:04 +0300

    Add first version webpack config

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const project_folder = path.basename(__dirname);
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry:
+    {
+      main: './#src/index.js'
+    },
+    output: {
+      path: path.resolve(__dirname, project_folder),
+      filename: '[name].[contenthash].js',
+      clean: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./#src/index.pug",
+        favicon: "./#src/favicon.ico"
+      }),
+      new MiniCssExtractPlugin()
+    ],
+    devServer: {
+      watchFiles: {
+        paths: ['#src/**/*.*'],
+        options: {
+          usePolling: false,
+        },
+      },
+      port: 9000,
+    },
+    module: {
+      rules: [
+        // pug
+        {
+          test: /\.pug$/,
+          use: [
+            {
+              loader: "pug-loader",
+              options: {
+                pretty: true
+              }
+            }
+          ]
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+        }
+
+      ]
+    }
+ }
